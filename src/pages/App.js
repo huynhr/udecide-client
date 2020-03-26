3bd0be4e99cc6f855830ef2181f96df51a5090f7
Ray Huynh <huynh.r.k@gmail.com>
2020-03-26 17:47:09 -0600

    Reconfigure folders

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+@tailwind components;
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello world from React!</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
